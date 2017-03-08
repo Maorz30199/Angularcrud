bd432ab485d3bb876633198e249e7b73c9f6e393
Maorz30199 <cesarruiz@unicauca.edu.co>
2017-03-07 20:52:21 -0500

    Crud Angularjs
    Creación de usuarios, editar y eliminar, Estilos de bootstrap

---
+++ js/app.js
@@ -0,0 +1,28 @@
+angular.module("myApp",[])
+.controller("myController",function($scope){
+  $scope.newUser ={};
+  $scope.clickedUser = {};
+  $scope.message = "";
+
+  $scope.users = [
+    {username: "Maorz", fullName: "Cesar Mauricio Ruiz Tulande", email:"Maorz314@gmail.com"},
+    {username: "Kellymupe", fullName: "Kelly Muñoz", email:"keyos1994@gmail.com"}
+  ];
+
+  $scope.saveUser = function(){
+    $scope.users.push($scope.newUser);
+    $scope.newUser ={};
+  };
+
+  $scope.selectUser = function(user){
+    $scope.clickedUser = user;
+  };
+
+  $scope.updateUser = function(){
+
+  };
+
+  $scope.deleteUser = function(){
+    $scope.users.splice($scope.users.indexOf($scope.clickedUser), 1);
+  };
+});
